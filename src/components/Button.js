37899d0de9b91113bb25344c7abbf7fd90d6b20a
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-25 10:11:51 +0700

    Access Data with Consumers

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,9 +7,16 @@ class Button extends React.Component {
 
   render() {
     // console.log(this.context);
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
+    // const text = this.context === "english" ? "Submit" : "Voorleggen";
 
-    return <button className="ui button primary">{text}</button>;
+    // return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
